026dcdba6e8054a995f083f446f70d97e3613a55
Andres Bonilla Gomez <abraham.go@outlook.com>
2021-01-28 21:15:07 -0600

    Init commit

---
+++ Apps/CleanMonoSwift/CleanMonoSwift/CleanMonoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanMonoSwiftApp.swift
+//  CleanMonoSwift
+//
+//  Created by Andrés Bonilla Gómez on 28/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CleanMonoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Data/Gateways/CoreDataSource/CoreDataSource/CoreDataSource.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataSource.h
+//  CoreDataSource
+//
+//  Created by Andrés Bonilla Gómez on 28/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataSource.
+FOUNDATION_EXPORT double CoreDataSourceVersionNumber;
+
+//! Project version string for CoreDataSource.
+FOUNDATION_EXPORT const unsigned char CoreDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataSource/PublicHeader.h>
+
+
